de19c1aca526f112b1e36e18999eab7e67036539
Environmental Logging Equipment OPEN SOURCE <host@downtrodden.16mb.com>
2016-06-26 17:18:00 +0100

    Update M590_Arduino.ino

--- M590_Arduino.ino
+++ M590_Arduino.ino
@@ -2,19 +2,22 @@
 //  Rewrite for M590 GPRS module
 //  http://www.banggood.com/GSM-GPRS-SIM900-1800MHz-Short-Message-Service-m590-SMS-Module-DIY-Kit-For-Arduino-p-1043437.html
 //  
-//  Added Nunber Verification
+//  Added Nunber Verification (Full International Number required)
+//  Added Response to incomming number, useful for returning sensor data etc...
 //  The Power Supply needs to be able to produce 5v 2A for RF to function(After Drop over diode ~4.5v)
 //  If there are any problems, the PSU is the most likely cause, 
 //  Only UK Networks tested
-//  the SIM choice can also cause issues, EE caused numerous failures
-//  but O2 seems to connect every time, although the SMS Delivery can take Longer 
+//  the SIM choice can also cause issues, EE caused numerous failures, Vodafone failed about 20%
+//  but Tesco/O2 seems to connect every time, although the SMS Delivery can take Longer 
+ 
+
  
 #include <SoftwareSerial.h> 
-char inchar;                    // Will hold the incoming character from the GSM shield
-String conTrol = "00000000000"; // Only allow control from this number....
-SoftwareSerial M590(7, 8);      // Rx, Tx
-String callID = "";
-String StrCallID = "";
+char inchar;                     // Will hold the incoming character from the GSM shield
+String conTrol = "XXXXXXXXXXXX"; // Only allow control from this number.... Include Country Code ! 447700000000
+SoftwareSerial M590(7, 8);       // Rx, Tx
+String callID = "";              // Line of Text to parse
+String StrCallID = "";           
 String StrCallID1 = "";
 int led1 = 10;
 int led2 = 11;
@@ -37,9 +40,11 @@ void setup()
  
   // wake up the GSM shield
 M590.begin(9600);
- // give time to log on to network.
+ // log on to cell network in the sketch.
+ // AT+CFUN=15 will reset the module, then set normal mode AT+CFUN=1
+ // Module returns +PBREADY on connection........
 delay(5500);
-M590.print("ATE0\r");
+M590.print("ATE0\r");  // Echo off, I had some issues.....
 delay(500);
 M590.print("AT+CMGF=1\r");  // set SMS mode to text
 Serial.println("set SMS mode to txt");  // set SMS mode to text
@@ -84,8 +89,8 @@ void smsG()
      Serial.println(callID);
      StrCallID = callID.substring(1,4);
     if (StrCallID =="CMT"){
-      StrCallID1 = "0";
-      StrCallID1 += callID.substring(10,20);
+      StrCallID1 = "";
+      StrCallID1 += callID.substring(8,20);
       Serial.print("The Caller ID to Verify : ");
       Serial.println(StrCallID1);
     if (StrCallID1 == conTrol){
@@ -164,7 +169,12 @@ void smsG()
               inchar=M590.read(); 
               if (inchar=='0')
               {
-
+              M590.print("AT+CMGS=\"" + StrCallID1 + "\"\r\n");  // Number to reply to from incomming message
+              delay(1500);
+              M590.print("HELLO WORLD....");//message content
+              delay(500);
+              M590.write(0x1a);     // send out SMS
+              delay(1000);
               } 
               else if (inchar=='1')
               {
@@ -184,3 +194,5 @@ void loop()
   }
     //This is where to do some interesting stuff......
 }
+    
+  
