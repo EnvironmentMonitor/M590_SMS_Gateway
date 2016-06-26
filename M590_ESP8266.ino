a081191aea2f0ac117e47a278095601de46cded3
Environmental Logging Equipment OPEN SOURCE <host@downtrodden.16mb.com>
2016-06-26 18:08:25 +0100

    Update M590_ESP8266.ino
    Added Sensor Data SMS Reply

--- M590_ESP8266.ino
+++ M590_ESP8266.ino
@@ -2,12 +2,14 @@
 //  Rewrite for M590 GPRS module
 //  http://www.banggood.com/GSM-GPRS-SIM900-1800MHz-Short-Message-Service-m590-SMS-Module-DIY-Kit-For-Arduino-p-1043437.html
 //  
-//  Added Nunber Verification
+//  Added Nunber Verification (Full International Number required)
+//  Added Response to incomming number, useful for returning sensor data etc...
+//  Added Example sensor reply......
 //  The Power Supply needs to be able to produce 5v 2A for RF to function(After Drop over diode ~4.5v)
 //  If there are any problems, the PSU is the most likely cause, 
 //  Only UK Networks tested
-//  the SIM choice can also cause issues, EE caused numerous failures
-//  but O2 seems to connect every time, although the SMS Delivery can take Longer 
+//  the SIM choice can also cause issues, EE caused numerous failures, Vodafone failed about 20%
+//  but Tesco/O2 seems to connect every time, although the SMS Delivery can take Longer 
  
 #include <SoftwareSerial.h> 
 char inchar;                    // Will hold the incoming character from the GSM shield
@@ -37,9 +39,11 @@ void setup()
  
   // wake up the GSM shield
 M590.begin(9600);
-  // give time to log on to network.
+ // log on to cell network in the sketch.
+ // AT+CFUN=15 will reset the module, then set normal mode AT+CFUN=1
+ // Module returns +PBREADY on connection........
 delay(5500);
-M590.print("ATE0\r");
+M590.print("ATE0\r");  // Echo off, I had some issues.....
 delay(500);
 M590.print("AT+CMGF=1\r");  // set SMS mode to text
 Serial.println("set SMS mode to txt");  // set SMS mode to text
@@ -84,8 +88,8 @@ void smsG()
      Serial.println(callID);
      StrCallID = callID.substring(1,4);
     if (StrCallID =="CMT"){
-      StrCallID1 = "0";
-      StrCallID1 += callID.substring(10,20);
+      StrCallID1 = "";
+      StrCallID1 += callID.substring(8,20);
       Serial.print("The Caller ID to Verify : ");
       Serial.println(StrCallID1);
     if (StrCallID1 == conTrol){
@@ -164,8 +168,20 @@ void smsG()
               inchar=M590.read(); 
               if (inchar=='0')
               {
-
-              } 
+              delay(250);
+              int analogV = analogRead(A0);
+              String analogS = "";
+              analogS += analogV;
+              delay(250);
+              M590.print("AT+CMGS=\"" + StrCallID1 + "\"\r\n");  // Number to reply to from incomming message
+              delay(1500);
+              M590.print("Sensor Value = ");//message content
+              delay(500);
+              M590.print(analogS);//message content
+              delay(500);
+              M590.write(0x1a);     // send out SMS
+              delay(1000);
+              }
               else if (inchar=='1')
               {
              Serial.println("delete all SMS....");
@@ -184,3 +200,5 @@ void loop()
   }
     //This is where to do some interesting stuff......
 }
+    
+  
